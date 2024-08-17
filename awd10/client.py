998adf78ab93c112a2779f1a2c0c0f6a24e862fe
Ryadno Alexey <aryadno@mail.ru>
2024-08-17 10:31:55 +0300

    refactoring

--- awd10/client.py
+++ awd10/client.py
@@ -5,29 +5,33 @@
 """
 
 import logging
+from enum import IntEnum
 
 from serial import Serial
+
+from .device import AWD10
 
 _logger = logging.getLogger(__name__)
 _logger.addHandler(logging.NullHandler())
 
 
-# Коды команд управления (таблица 6 документации)
-CMD_ECHO = 0xF0
-CMD_EXEC_CMD = 0x4B
-CMD_GET_PARAM = 0x87
-CMD_GET_RESULT = 0x3C
-CMD_SET_PARAM = 0x78
-
-# Номера параметров для команды CMD_EXEC_CMD (таблица 8 документации)
-CMD_ENROT = 0x0B
-CMD_RESET = 0x09
-CMD_SETROT = 0x08
-CMD_STOP = 0x0A
-
-
 class AwdProtocolError(Exception):
     pass
+
+
+class CMD(IntEnum):     # таблицы 6 и 8 документации
+    """Команды управления."""
+
+    ECHO = 0xF0
+    EXEC_CMD = 0x4B
+    GET_PARAM = 0x87
+    GET_RESULT = 0x3C
+    SET_PARAM = 0x78
+
+    ENROT = 0x0B
+    RESET = 0x09
+    SETROT = 0x08
+    STOP = 0x0A
 
 
 class Client:
@@ -35,14 +39,12 @@ class Client:
     постоянного тока AWD10.
     """
 
-    def __init__(self, port: str, unit: int,
-                       device: dict, timeout: float = 1.0) -> None:
+    def __init__(self, port: str, unit: int, timeout: float = 1.0) -> None:
         """Инициализация класса клиента с указанными параметрами."""
 
-        self.socket = Serial(port=port, timeout=timeout)
+        self.socket = Serial(port=port, timeout=timeout)    # default 9600-8-N-1
         self.port = port
         self.unit = unit
-        self.device = device
 
     def __del__(self) -> None:
         """Закрытие соединения с устройством при удалении объекта."""
@@ -53,10 +55,10 @@ class Client:
     def __repr__(self) -> str:
         """Строковое представление объекта."""
 
-        return f"Client(port={self.port}, unit={self.unit})"
+        return f"{type(self).__name__}(port={self.port!r}, unit={self.unit})"
 
     @staticmethod
-    def _error_check(request: bytes, answer: bytes) -> bool:
+    def _check_error(request: bytes, answer: bytes) -> bool:
         """Проверка возвращаемого значения на ошибку."""
 
         if len(answer) < 8:
@@ -69,6 +71,16 @@ class Client:
             msg = f"unit {answer[0]} error code {answer[1]:02X}"
             raise AwdProtocolError(msg)
         return True
+
+    @staticmethod
+    def _check_name(arg: str, name: str) -> dict:
+        """Проверка названия параметра."""
+
+        if name not in AWD10[arg]:
+            msg = f"Unknown parameter '{name}'"
+            raise AwdProtocolError(msg)
+
+        return AWD10[arg][name]
 
     def _make_packet(self, command: int, param: int, data: int) -> bytes:
         """Формирование пакета для записи."""
@@ -96,33 +108,33 @@ class Client:
         answer = self._bus_exchange(packet)
         _logger.debug("Recv frame = %s", list(answer))
 
-        self._error_check(packet, answer)
+        self._check_error(packet, answer)
         return answer
 
     def get_param(self, name: str) -> int:                  # Таблица 7 документации
         """Чтение значения параметра по заданному имени."""
 
-        return self._get_value("param", name, CMD_GET_PARAM)
+        return self._get_value("param", name, CMD.GET_PARAM)
 
     def set_param(self, name: str, value: int) -> bool:     # Таблица 7 документации
         """Запись значения параметра по заданному имени."""
 
-        dev = self.device["param"][name]
+        dev = self._check_name("param", name)
         if value not in range(dev["min"], dev["max"] + 1):
             msg = f"An '{name}' value of '{value}' is out of range"
             raise AwdProtocolError(msg)
 
-        return bool(self._send_message(CMD_SET_PARAM, dev["code"], value))
+        return bool(self._send_message(CMD.SET_PARAM, dev["code"], value))
 
     def move(self, speed: int = 0) -> bool:
         """Движение с постоянной скоростью. Знак скорости определяет направление."""
 
-        return bool(self._send_message(CMD_EXEC_CMD, CMD_SETROT, speed & 0xFFFF))
+        return bool(self._send_message(CMD.EXEC_CMD, CMD.SETROT, speed & 0xFFFF))
 
     def state(self) -> dict:        # п.2.5.4.4 и 2.5.4.5 документации
         """Чтение состояния флагов режима работы платы."""
 
-        answer = self._send_message(CMD_GET_PARAM, 0x1C, 0x0000)
+        answer = self._send_message(CMD.GET_PARAM, 0x1C, 0x0000)
         return {"FB":          bool(answer[4] >> 7 & 1),
                 "SkipLim":     bool(answer[4] >> 6 & 1),
                 "LimDrop":     bool(answer[4] >> 5 & 1),
@@ -145,34 +157,34 @@ class Client:
     def reset(self) -> bool:
         """Все параметры сбрасываются, движение прекращается."""
 
-        return bool(self._send_message(CMD_EXEC_CMD, CMD_RESET, 0x0000))
+        return bool(self._send_message(CMD.EXEC_CMD, CMD.RESET, 0x0000))
 
     def echo(self) -> bool:
         """Посылка Echo-запроса. Если устройство доступно возвратится True."""
 
-        answer = self._send_message(CMD_ECHO, 0x0000, 0x0000)
+        answer = self._send_message(CMD.ECHO, 0x0000, 0x0000)
         return tuple(answer[2:5]) == (0x41, 0x57, 0x44)
 
     def stop(self) -> bool:
         """Закончить выполнение режима."""
 
-        return bool(self._send_message(CMD_EXEC_CMD, CMD_STOP, 0x0000))
+        return bool(self._send_message(CMD.EXEC_CMD, CMD.STOP, 0x0000))
 
     def enrot(self) -> bool:
         """Включить режим слежения за внешним аналоговым сигналом."""
 
-        return bool(self._send_message(CMD_EXEC_CMD, CMD_ENROT, 0x0000))
+        return bool(self._send_message(CMD.EXEC_CMD, CMD.ENROT, 0x0000))
 
     def result(self, name: str) -> int:             # Таблица 9 документации
         """Чтение параметров состояния двигателя и блока управления."""
 
-        return self._get_value("result", name, CMD_GET_RESULT)
+        return self._get_value("result", name, CMD.GET_RESULT)
 
     def _get_value(self, arg: str, name: str, cmd: int) -> int:
         """Чтение текущего параметра или состояния двигателя."""
 
-        param = self.device[arg][name]["code"]
-        answer = self._send_message(cmd, param, 0)
+        dev = self._check_name(arg, name)
+        answer = self._send_message(cmd, dev["code"], 0)
         return answer[4] << 8 | answer[5]
 
 
--- example/example.py
+++ example/example.py
@@ -6,13 +6,12 @@ import logging
 from time import sleep
 
 from awd10.client import Client
-from awd10.device import AWD10
 
 logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == "__main__":
-    client = Client(port="COM1", unit=5, device=AWD10)
+    client = Client(port="COM1", unit=5)
     print(client)
 
     print(f"Move: {client.move(speed=100)}")
